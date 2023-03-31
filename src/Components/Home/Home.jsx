ab548431076e76aa255dcfc897e1b83808a9e3d2
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-03-31 15:17:27 +0600

    fetching data and pass to singlblog section from home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import SingleBlog from "../SingleBlog/SingleBlog";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const res = await fetch("data.json");
+      const data = await res.json();
+      console.log(data);
+      setBlogs(data);
+    };
+    loadData();
+  }, []);
+
   return (
     <div className="w-full">
-      <h1>Home</h1>
+      {blogs.map((blog) => (
+        <SingleBlog blog={blog} key={blog.id} />
+      ))}
     </div>
   );
 };
---
+++ src/Components/SingleBlog/SingleBlog.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SingleBlog = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default SingleBlog;
